9cf9813804552d11f579f5d402c03fa46b156758
0xAnon <utkarshshail005@protonmail.com>
2024-03-22 04:13:08 +0530

    More advanced version

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { Background } from '@vue-flow/background'
-import { Position, VueFlow, MarkerType } from '@vue-flow/core'
+import { Position, VueFlow, MarkerType, useVueFlow } from '@vue-flow/core'
 import axios from 'axios'
+
 
 const IYP_API_BASE = 'https://iyp.iijlab.net/iyp/db/neo4j/tx/'
 const DEFAULT_TIMEOUT = 180000
@@ -36,21 +37,34 @@ function calculate(input) {
 }
 
 const nodes = ref([])
+const allNodes = ref([])
+const allNodesPosition = ref([])
+const allRelation = ref([])
 
 const edges = ref([])
 
 const as_info_query = ref({
   loading: true,
-  query: `MATCH (a:AS {asn: $asn})-[d:DEPENDS_ON]->(b:AS)
-          WHERE a<>b 
-          RETURN d.hege*100 AS Hege, b.asn AS B`,
-})
+  query:`MATCH (a:AS {asn: $asn})-[h:DEPENDS_ON {af:4}]->(d:AS)
+          WITH a, COLLECT(DISTINCT d) AS dependencies
+          UNWIND dependencies as d
+          MATCH p = allShortestPaths((a)-[:PEERS_WITH*]-(d))
+          WHERE a.asn <> d.asn AND all(r IN relationships(p) WHERE r.af = 4) AND all(n IN nodes(p) WHERE n IN dependencies)
+          RETURN p`
+})    
 
 const searchASN = async() => {
+
+  allNodes.value = []
+  allNodesPosition.value = []
+  nodes.value = []
+  allRelation.value = []
+  edges.value = []
 
   let response = await axios_base.post('', {
     statements: [{statement: as_info_query.value.query, parameters:{ asn: Number(asn.value) }}]
   })
+
 
   const rows = response.data.results
   const res = []
@@ -58,46 +72,172 @@ const searchASN = async() => {
     res.push(formatResponse(rows[i]))
   }
 
-  sortNodes(res[0])
+  for(const result of res){
 
-}
+    for(const connection of result) {
 
-const sortEdges = (res) => {
+      let nodeCount = 0
+      let previousASN = Number(asn.value)
+      
+      for(const finalConnection of connection.p) {
 
-  let index = 1
-  while(index != nodes.value.length) {
-      edges.value.push({ id: `e1-${nodes.value[index].id}`, label: `${res[index-1].Hege.toFixed(2)}%`, source: '1', target: nodes.value[index].id, animated: true, markerEnd: MarkerType.ArrowClosed})
-      index++
-  } 
+        if((Object.keys(finalConnection)).length != 1 ){
 
-}
+          allRelation.value.push([finalConnection.asn1, finalConnection.asn2])
 
-const sortNodes = (res) => {
+          if(allNodes.value[nodeCount] == undefined) {allNodes.value[nodeCount] = []}
+          
+            if (finalConnection.asn1 !== previousASN) {
 
-  if(res.length > 0) {
+              if(!(allNodes.value[nodeCount].includes(finalConnection.asn1))){
 
-    nodes.value = []
-    edges.value = []
+                if(nodeCount!=0){
+                  if(!(allNodes.value[nodeCount-1].includes(finalConnection.asn1))){
+                    allNodes.value[nodeCount].push(finalConnection.asn1);
+                    previousASN = finalConnection.asn1
+                  }
+                }else {
+                    allNodes.value[nodeCount].push(finalConnection.asn1);
+                    previousASN = finalConnection.asn1
+                }
+              
+              }
+            
+            }
 
-    nodes.value.push( {id: '1', type: 'input', label: `AS${asn.value}`, position: { x: -100, y: calculate(res.length) } , sourcePosition: Position.Right, dimensions: {"height":0, "width":0}})
-  
-    let y_axis = 0
-    let id = 2
+            else if (finalConnection.asn2 !== previousASN) {
 
-    for(const node of res) {
-      nodes.value.push( {id: String(id), type: 'output',label: `AS${String(node.B)}`, position: { x: 400, y: y_axis } , targetPosition: Position.Left})
-      y_axis+=50
-      id++
+              if(!(allNodes.value[nodeCount].includes(finalConnection.asn2))){
+
+                if(nodeCount!=0){
+                  if(!(allNodes.value[nodeCount-1].includes(finalConnection.asn2))){
+                    allNodes.value[nodeCount].push(finalConnection.asn2);
+                    previousASN = finalConnection.asn2
+                  }
+                }else {
+                    allNodes.value[nodeCount].push(finalConnection.asn2);
+                    previousASN = finalConnection.asn2
+                }
+
+              }            
+            }
+
+
+          nodeCount++;
+
+        }
+
+      }
+
     }
 
-    sortEdges(res)
+  }
+
+  const indexOfLargest = allNodes.value.reduce((maxIndex, currentElement, currentIndex, array) => {
+  return currentElement.length > array[maxIndex].length ? currentIndex : maxIndex;}, 0); 
+
+  allNodesPosition.value = Array.from({ length: allNodes.value.length }, () => []);
+
+  let position_y = 0
+  
+  for(let i=0; i!= allNodes.value[indexOfLargest].length; i++) {
+    allNodesPosition.value[indexOfLargest].push(position_y)
+    position_y += 50
+  }
+
+  for(let i=0; i!=allNodesPosition.value.length; i++) {
+
+    if(i != indexOfLargest) {
+
+      const increase = position_y /( allNodes.value[i].length + 1)
+      let inital_y = increase
+
+      for(let j=0; j!= allNodes.value[i].length; j++) {
+        allNodesPosition.value[i].push(inital_y)
+        inital_y += increase
+      }      
+
+    }
+
+  }
+
+  sortNodes(indexOfLargest)
 
 }
+
+const sortEdges = () => {
+
+  for(const rel of allRelation.value) {
+
+    let index1 = 0
+    let index2 = 0
+
+    for(let i=0;i!=allNodes.value.length;i++){
+
+      if(allNodes.value[i].includes(rel[0])){
+        index1 = i+1
+      }
+
+      if(allNodes.value[i].includes(rel[1])){
+        index2 = i+1
+      }
+
+    }
+
+
+    if(index2 > index1){
+      edges.value.push({ id: `${rel[0]}-${rel[1]}`, source: String(rel[0]), target: String(rel[1]), animated: true})
+    }
+    else if(index1 > index2){
+      edges.value.push({ id: `${rel[1]}-${rel[0]}`, source: String(rel[1]), target: String(rel[0]), animated: true})
+    }
+
+  
+  }
+
+}
+
+const sortNodes = (index) => {
+
+  nodes.values = []
+  nodes.value.push({id: asn.value, type: 'input', label: `AS${asn.value}`, position: { x: 0, y: calculate(allNodes.value[index].length) } , sourcePosition: Position.Right})
+
+  let x_index = 300
+
+  for(let i=0 ; i!= allNodes.value.length ; i++) {
+
+    if(allNodes.value[i].length == 1){
+
+      for(let j=0 ; j!= allNodes.value[i].length ; j++) {
+        nodes.value.push({id: String(allNodes.value[i][j]), type: 'output', label: `AS${allNodes.value[i][j]}`, position: { x: x_index, y: calculate(allNodes.value[index].length) }, targetPosition: Position.Left})
+      }
+
+    }
+
+    else if(i == allNodes.value.length - 1){
+
+      for(let j=0 ; j!= allNodes.value[i].length ; j++) {
+        nodes.value.push({id: String(allNodes.value[i][j]), type: 'output', label: `AS${allNodes.value[i][j]}`, position: { x: x_index, y: allNodesPosition.value[i][j] }, targetPosition: Position.Left})
+      }
+
+    }else{
+
+      for(let j=0 ; j!= allNodes.value[i].length ; j++) {
+        nodes.value.push({id: String(allNodes.value[i][j]), type: 'default', label: `AS${allNodes.value[i][j]}`, position: { x: x_index, y: allNodesPosition.value[i][j] }, sourcePosition: Position.Right, targetPosition: Position.Left})
+      }
+
+    }
+
+    x_index += 300
+
+  }
+
+  sortEdges()
 
 }
 
 onMounted(() => {
-  asn.value = "2497"
+  asn.value = "174"
   searchASN()
 })
 
@@ -111,8 +251,8 @@ onMounted(() => {
     <q-btn @click=searchASN() color="primary" label="Search"  style="min-width: 120px; margin-left:20px;"/>
   </div>
 
-  <VueFlow style="width: 900px; height: 600px"  class="vueTest" :nodes="nodes" :edges="edges" fit-view-on-init>
-    <Background   :gap=6  />
+  <VueFlow style="width: 900px; height: 600px" class="vueTest" :nodes="nodes" :edges="edges" fit-view-on-init>
+    <Background :gap=6 />
   </VueFlow>
 
 
@@ -139,3 +279,4 @@ onMounted(() => {
 
 
 </style>
+
